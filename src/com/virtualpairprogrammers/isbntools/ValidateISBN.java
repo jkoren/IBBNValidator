8e112334dec00efaf8e58de363c84f5806ea53c6
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-19 16:29:06 -0500

    getting to red

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,4 +2,7 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
+    public boolean checkISBN(int i) {
+        return false;
+    }
 }
--- test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -1,9 +1,15 @@
 package com.virtualpairprogrammers.isbntools;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ValidateISBNTest {
+    @Test
     public void checkAValidISBN() {
-        fail();
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN(140449116);
+        assertTrue(result);
     }
 }
