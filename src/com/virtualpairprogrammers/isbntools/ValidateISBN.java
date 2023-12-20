edbccb64c99c121da8096d6219f43971f437aa30
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-20 10:44:00 -0500

    add test and code to throw exception if number is not 10 digits long

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,8 +1,9 @@
 package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
-
     public boolean checkISBN(String isbn) {
+
+        if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits long - booyah");
         int total = 0;
         for (int i = 0; i < 10; i++) {
             total += isbn.charAt(i) * (10 - i);
--- test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -11,7 +11,6 @@ public class ValidateISBNTest {
 //        assertTrue(result); // this works, but can add text to be descriptive if we want)
         assertTrue(result, "first value");
         result = validator.checkISBN("0140177396");
-//        assertTrue(result); // this works, but can add text to be descriptive if we want)
         assertTrue(result, "second value");
     }
     @Test
@@ -19,5 +18,19 @@ public class ValidateISBNTest {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
+    }
+
+    @Test
+    public  void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        // JUnit 5 approach
+        assertThrows(NumberFormatException.class,
+                () -> {
+                validator.checkISBN("123456789");
+                // here we don't care about the result, only that we get an exception on improperly formatted number
+                // boolean result = validator.checkISBN("123456789");
+
+                }
+        );
     }
 }
