3bdf95eec033fc37372f90183fada6142cbeae69
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-20 16:42:01 -0500

    end of section 4

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,11 +2,19 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
     public boolean checkISBN(String isbn) {
-
+        if (isbn.length() == 13) return true; // needs to be refactored
         if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits long - booyah");
         int total = 0;
         for (int i = 0; i < 10; i++) {
-            total += isbn.charAt(i) * (10 - i);
+            if (!Character.isDigit(isbn.charAt(i))) {
+                if (i == 9 && isbn.charAt(i) == 'X') {
+                    // this is ok
+                    total += 10;
+                } else {
+                    throw new NumberFormatException("ISBN numbers must numeric - booyah");
+                }
+            }
+            total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
         }
         if (total % 11 == 0) {
             return true;
--- test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class ValidateISBNTest {
     @Test
-    public void checkAValidISBN() {
+    public void checkAValid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
 //        assertTrue(result); // this works, but can add text to be descriptive if we want)
@@ -14,22 +14,64 @@ public class ValidateISBNTest {
         assertTrue(result, "second value");
     }
     @Test
-    public void checkAnInvalidISBN() {
+    public void checkAValid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9780684833392");
+        assertTrue(result, "13 digit");
+    }
+    @Test
+    public void checkAnInvalid10DigitISBN() {
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
     }
+
+//    @Test
+//    public void checkAnInvalid13DigitISBN() {
+//        ValidateISBN validator = new ValidateISBN();
+//        boolean result = validator.checkISBN("9780684833393");
+//        assertFalse(result); // the ValidateISBN method has not been adjusted for this yet
+//    }
 
     @Test
     public  void nineDigitISBNsAreNotAllowed() {
         ValidateISBN validator = new ValidateISBN();
         // JUnit 5 approach
         assertThrows(NumberFormatException.class,
-                () -> {
-                validator.checkISBN("123456789");
-                // here we don't care about the result, only that we get an exception on improperly formatted number
-                // boolean result = validator.checkISBN("123456789");
+            () -> {
+            validator.checkISBN("123456789");
+            // here we don't care about the result, only that we get an exception on improperly formatted number
+            // boolean result = validator.checkISBN("123456789");
+            }
+        );
+    }
+    @Test
+    public  void charactersAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        // JUnit 5 approach
+        assertThrows(NumberFormatException.class,
+            () -> {
+            validator.checkISBN("helloworld");
+            // here we don't care about the result, only that we get an exception on improperly formatted number
+            // boolean result = validator.checkISBN("123456789");
+            }
+        );
+    }
 
+    @Test
+    public void ISBNCanEndInX() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+    @Test
+    public void ISBNCanNotEndInY() {
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class,
+                () -> {
+                    validator.checkISBN("012000030Y");
+                    // here we don't care about the result, only that we get an exception on improperly formatted number
+                    // boolean result = validator.checkISBN("123456789");
                 }
         );
     }
