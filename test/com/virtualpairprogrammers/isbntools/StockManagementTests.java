c0621a4ac91f6b35a0c859ae8af782dd0505f5c2
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-21 15:33:58 -0500

    create an initial failing test

---
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StockManagementTests {
+    @Test
+    public void test() {
+        fail("not yet implemented");
+    }
+}
