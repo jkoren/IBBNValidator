9771d11d66a687bb71e7542a8ea90e0440ff8592
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-21 15:18:57 -0500

    adding a constant to the method

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -1,9 +1,11 @@
 package com.virtualpairprogrammers.isbntools;
 
+
 public class ValidateISBN {
+    private static final int SHORT_ISBN_LENGTH = 10;
     public boolean checkISBN(String isbn) {
         if (isbn.length() == 13) return true; // needs to be refactored
-        if (isbn.length() != 10) throw new NumberFormatException("ISBN numbers must be 10 digits long - booyah");
+        if (isbn.length() != SHORT_ISBN_LENGTH) throw new NumberFormatException("ISBN numbers must be 10 digits long - booyah");
         int total = 0;
         for (int i = 0; i < 10; i++) {
             if (!Character.isDigit(isbn.charAt(i))) {
