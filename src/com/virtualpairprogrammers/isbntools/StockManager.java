7e9c1fc90288898567c641dbdef1ab70a97f33b4
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-26 12:01:39 -0500

    testing behavior as well as logic

--- src/com/virtualpairprogrammers/isbntools/StockManager.java
+++ src/com/virtualpairprogrammers/isbntools/StockManager.java
@@ -1,14 +1,27 @@
 package com.virtualpairprogrammers.isbntools;
 
 public class StockManager {
-    private ExternalISBNDataService service;
+    private ExternalISBNDataService webService;
 
-    public void setService(ExternalISBNDataService service) {
-        this.service = service;
+    public void setDatabaseService(ExternalISBNDataService databaseService) {
+        this.databaseService = databaseService;
     }
 
+    public void setWebService(ExternalISBNDataService webService) {
+        this.webService = webService;
+    }
+
+
+    private ExternalISBNDataService databaseService;
+
+
+//    public void setService(ExternalISBNDataService service) {
+//        this.service = service;
+//    }
+
     public String getLocatorCode(String isbn) {
-        Book book = service.lookup(isbn);
+        Book book = databaseService.lookup(isbn);
+        if (book == null) book = webService.lookup(isbn);
         StringBuilder locator = new StringBuilder();
         locator.append(isbn.substring(isbn.length() - 4));
         locator.append(book.getAuthor().substring(0,1));
--- test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -1,16 +1,31 @@
 package com.virtualpairprogrammers.isbntools;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class StockManagementTests {
     @Test
     public void testCanGetACorrectLocatorCode() {
-//        fail("not yet implemented");
-        String isbn = "9140177396";
+
+        ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                // for the purposes of this test, .lookup should always return null
+                return null;
+            }
+        };
+        ExternalISBNDataService testWebService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return new Book(isbn, "Of Mice and Men", "J. Steinbeck");
+            }
+        };
+
         StockManager stockManager = new StockManager();
+        stockManager.setWebService(testWebService);
+        stockManager.setDatabaseService(testDatabaseService);
+
+        String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         // check to see if the locator code we get back is what we think it should be
         assertEquals("7396J4", locatorCode);
