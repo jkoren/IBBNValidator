29c81203004929edc51b4075dd9222b19ac3eed2
jeffkorenstein <korenstein_jeff@bah.com>
2023-12-26 18:04:47 -0500

    refactored and simplified started but not finished

--- test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -8,47 +8,34 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
+    // declare at class level ...
+    ExternalISBNDataService testWebService;
+    ExternalISBNDataService testDatabaseService;
+    StockManager stockManager;
     @BeforeEach
     public void setup() {
+        // ... but instantiate at set up
         System.out.println("set up running");
+        testWebService = mock(ExternalISBNDataService.class);
+        StockManager stockManager = new StockManager();
+        testDatabaseService = mock(ExternalISBNDataService.class);
+        stockManager.setDatabaseService(testDatabaseService);
+        stockManager.setWebService(testWebService);
     }
     @AfterEach
     public void teardown() {
         System.out.println("tear-down running");
     }
-    @Test
-    public void testCanGetACorrectLocatorCode() {
-
-//        ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
-//            @Override
-//            public Book lookup(String isbn) {
-//                // for the purposes of this test, .lookup should always return null
-//                return null;
-//            }
-//        };
-////        ExternalISBNDataService testWebService = new ExternalISBNDataService() {
-////            @Override
-////            public Book lookup(String isbn) {
-////                return new Book(isbn, "Of Mice and Men", "J. Steinbeck");
-////            }
-////        };
-//
-//        StockManager stockManager = new StockManager();
-//        stockManager.setWebService(testWebService);
-//        stockManager.setDatabaseService(testDatabaseService);
+//    @Test
+//    public void testCanGetACorrectLocatorCode() {
+//        when(testWebService.lookup(anyString())).thenReturn(new Book("0140177396", "Of Mice and Men", "J. Steinbeck"));
+//        when(testDatabaseService.lookup(anyString())).thenReturn(null);
 //
 //        String isbn = "0140177396";
 //        String locatorCode = stockManager.getLocatorCode(isbn);
 //        // check to see if the locator code we get back is what we think it should be
 //        assertEquals("7396J4", locatorCode);
-
-        // *** exercise - create above as a mock
-        ExternalISBNDataService testWebService = mock(ExternalISBNDataService.class);
-        when(testWebService.lookup(anyString())).thenReturn(new Book("0140177396", "Of Mice and Men", "J. Steinbeck"));
-        ExternalISBNDataService testDataBaseService = mock(ExternalISBNDataService.class);
-        when(testDataBaseService.lookup(anyString())).thenReturn(null);
-        // but this does not return a locator code, so changes the meaning of this test ?
-    }
+//    }
 
     @Test
     public void databaseIsUsedIfDataIsPresent() {
